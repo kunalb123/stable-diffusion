196e8a1fdde79625fabc9861ddae64282c0f0eef
Likith <lpalabin@ucsd.edu>
2024-03-13 18:28:32 -0700

    embeddings are working properly

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ class CLIPTextEmbedder(nn.Module):
 
     def __init__(self, version: str = "openai/clip-vit-large-patch14", device="cuda:0", max_length: int = 77):
         super().__init__()
-        self.tokenizer = CLIPTokenizer.from_pretrained(version)
-        self.transformer = CLIPTextModel.from_pretrained(version).eval()
+        self.tokenizer = CLIPTokenizer.from_pretrained(version, torch_dtype=torch.float16)
+        self.transformer = CLIPTextModel.from_pretrained(version, torch_dtype=torch.float16).to(device)
         self.device = device
         self.max_length = max_length
 
@@ -23,13 +23,14 @@ class Diffusion(nn.Module):
     def __init__(self, config):
         self.vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", torch_dtype=torch.float16)
         self.unet = UNet2DConditionModel()
-        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16)
+        self.tokenizer = CLIPTextEmbedder()
 
     def forward(self, image, text, timestep):
         latent_image = self.vae.encode(image)
-        latent_text = self.tokenizer(text, return_tensors="pt").input_ids
+        latent_text = self.tokenizer(text)
         return self.unet(latent_image, latent_text, timestep)
     
+
 clip = CLIPTextEmbedder()
-prompt = ["a photo of a cat"]
-print(clip(prompt))
+prompt = ["a photo of a cat", "a photo of a dog"]
+print(clip(prompt).shape)
